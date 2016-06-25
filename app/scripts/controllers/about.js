b64fd0d61be6e8cc6d0314cd0a1e80363ef0f909
mapineda <mapineda1222@gmail.com>
2016-06-25 15:08:09 -0500

    updated

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xeritopiaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the xeritopiaApp
+ */
+angular.module('xeritopiaApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xeritopiaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the xeritopiaApp
+ */
+angular.module('xeritopiaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
